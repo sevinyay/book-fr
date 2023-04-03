d19e846fb48fb82c094ca0505544fe3b117de102
sevinyay <114470480+sevinyay@users.noreply.github.com>
2023-04-03 23:17:16 +0300

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -16,12 +16,12 @@ const About = () => {
 How do I add the favorites books and update them through MEDTECH Book Shelf ? </p> 
 <p>         
 <b>Follow these steps:</b>
-<p> Click to open  <a href= "http://localhost:3000/books" > All Books</a>  on  MED Book Shelf </p>
+<p> Click to open  <a href= "https://book-full.onrender.com/books" > All Books</a>  on  MED Book Shelf </p>
 <p> Check if here is not your favorite book. Let's say it's not; </p>
-<p>Quickly go to  <a href="http://localhost:3000/add"> Add Books</a>  page. </p>
+<p>Quickly go to  <a href="https://book-full.onrender.com/add"> Add Books</a>  page. </p>
 <p>Observe the form which will let you to add your favorite one. </p>
 <p> Enter the required information that has been asked.</p>
-<p>Once you click the button can see your favorite book at the bottom of the<a href= "http://localhost:3000/books" > list.</a> </p>
+<p>Once you click the button can see your favorite book at the bottom of the<a href= "https://book-full.onrender.com/books" > list.</a> </p>
 <p>Enjoy with the list and share with your loved ones. </p>
 <p color="#2A4E6B"> The Best... </p>
 
